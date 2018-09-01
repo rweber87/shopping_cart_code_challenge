717dbb3f181e49b031aa0d8014f33288aabe4d52
Rob Weber <rob.weber87@gmail.com>
2018-09-01 12:37:56 -0400

    skeleton f project

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <div >
+        <p >
+          Hello World!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ src/inventory.js
@@ -0,0 +1,39 @@
+import tshirt_blue from './imagess/tshirt_blue.jpg';
+import tshirt_green from './images/tshirt_green.jpg';
+import tshirt_red from './images/tshirt_red.jpg';
+import tshirt_yellow from './images/tshirt_yellow.jpg';
+
+const inventory = {
+	{
+		name: "Cotton T-Shirt"
+		images: [tshirt_blue, tshirt_green, tshirt_red, tshirt_yellow],
+		sizes: ["S", "M", "L", "XL", "XXL"],
+		colors: ["blue", "red", "yellow", "green"],
+		full_price: 11.00,
+		discount_reduction: 0,
+	}
+	// {
+	// 	name: "Cotton T-Shirt"
+	// 	images: "",
+	// 	sizes: ["S", "M", "L", "XL", "XXL"],
+	// 	colors: ["blue", "red", "yellow", "green"],
+	// 	full_price: 17.00,
+	// 	discount_price: 0,
+	// },
+	// {
+	// 	name: "Cotton T-Shirt"
+	// 	images: "",
+	// 	sizes: ["S", "M", "L", "XL", "XXL"],
+	// 	colors: ["blue", "red", "yellow", "green"],
+	// 	full_price: 21.00,
+	// 	discount_price: 12.00,
+	// },
+	// {
+	// 	name: "Cotton T-Shirt"
+	// 	images: "",
+	// 	sizes: ["S", "M", "L", "XL", "XXL"],
+	// 	colors: ["blue", "red", "yellow", "green"],
+	// 	full_price: 22.00,
+	// 	discount_price: 0,
+	// }
+}
